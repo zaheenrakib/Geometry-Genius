83db1e18afcaa3a1ee2701e7b6ef29dba4205970
zaheenrakib <rakibislambest@gmail.com>
2024-02-12 23:17:29 +0600

    add rectangle

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,22 @@
 function calculateRectangleArea(){
-    console.log("buaj");
+    //get length of the rectangle
+
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    //get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+    //Calculate rectangle area
+    const area = length * width;
+    console.log('area of the rectangle:',area);
+
+    //display rectangle area 
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
